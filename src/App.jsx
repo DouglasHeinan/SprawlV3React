c58300eddddd0c03026bbe63d67ec403969c44f7
gruvis41 <dougheinan@gmail.com>
2024-01-19 14:41:00 -0700

    created dash layout/header/footer and features folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout";
 import Public from "../components/Public";
 import Login from "../components/Login";
+import DashLayout from "../components/DashLayout";
 
 function App() {
 
@@ -11,9 +12,12 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
+
+        <Route path="dash" element={<DashLayout />}>
+          
+        </Route>
       </Route>
     </Routes>
-    // <></>
   );
 
 }
