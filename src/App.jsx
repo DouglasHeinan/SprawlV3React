0561cc273fffc43942f65a891644b5545c845119
gruvis41 <dougheinan@gmail.com>
2024-01-19 12:32:35 -0700

    created public and layout components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css'
+// import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Layout from "../components/Layout";
 
 function App() {
 
   return (
-    <></>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        
+      </Route>
+    </Routes>
+    // <></>
   );
 
 }
---
+++ src/components/Public.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+// import React from 'react'
+
+const Public = () => {
+    const content = (
+        <section>
+            <main>
+                <h1>Welcome to Sprawlopolis Score Sheet</h1>
+                <p>Record and save results of any game from the -Opolis family of games.</p>
+            </main>
+        </section>
+    );
+
+    return content;
+}
+
+export default Public
