6b8cb06d6b23fd61fd03346959527bcb43e33a61
gruvis41 <dougheinan@gmail.com>
2024-01-20 11:28:22 -0700

    Users and Scoresheets features created and applied to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Layout from "./components/Layout";
 import Public from "./components/Public";
 import Login from "./features/auth/Login";
 import DashLayout from "./components/DashLayout";
+import Welcome from "./features/auth/welcome";
+import ScoreSheet from "./features/scoreSheet/ScoreSheet";
+import Users from "./features/users/Users";
 
 function App() {
 
@@ -14,6 +17,14 @@ function App() {
         <Route path="login" element={<Login />} />
 
         <Route path="dash" element={<DashLayout />}>
+          <Route index element={<Welcome />} />
+          <Route path="scoreSheet">
+            <Route index element={<ScoreSheet />} />
+          </Route>
+          <Route path="users">
+            <Route index element={<Users />} />
+          </Route>
+          
 
         </Route>
       </Route>
---
+++ src/features/scoreSheet/ScoreSheet.jsx
@@ -0,0 +1,7 @@
+const ScoreSheet = () => {
+  return (
+    <h1>ScoreSheet</h1>
+  )
+}
+
+export default ScoreSheet
